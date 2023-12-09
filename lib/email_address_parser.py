24471ac8732bcd550b67f530f94479e7d900af93
githinjisamson1 <githinjisamson148@gmail.com>
2023-12-09 06:07:08 +0300

    Completed assignment

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,31 @@
 # your code goes here!
+
+# import module
+import re
+
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+        # considering comma/whitespace character
+        pattern = r"[,\s]"
+        regex = re.compile(pattern)
+        match = regex.split(self.email_addresses)
+        uniqueAddresses = list(set(match))
+        sortedAddresses = sorted(uniqueAddresses)
+        return sortedAddresses
+
+
+email_addresses = "john@doe.com, person@somewhere.org"
+parser = EmailAddressParser(email_addresses)
+
+print(parser.parse())
+
+# => ["john@doe.com", "person@somewhere.org"]
+
+email_addresses2 = "john@doe.com person@somewhere.org"
+parser2 = EmailAddressParser(email_addresses2)
+result2 = parser2.parse()
+print(result2)
